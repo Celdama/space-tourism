f8f69892605640489cf1457780f0a13d883003c4
Celdama <celdama.dev@gmail.com>
2022-03-18 18:44:46 +0100

    create css global style variable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import { GlobalStyle } from './GlobalStyle';
+
 function App() {
   return (
     <div className='App'>
       <h1>Space tourism</h1>
+      <GlobalStyle />
     </div>
   );
 }
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,4 +1,23 @@
 import { createGlobalStyle } from 'styled-components';
 import 'sanitize.css';
 
-export const GlobalStyle = createGlobalStyle``;
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --darkBlue: #0B0D17;
+    --purple: #D0D6F9;
+    --white: #fff;
+    --heading1: 150px 'Bellefair', serif;
+    --heading2: 100px 'Bellefair', serif;
+    --heading3: 56px 'Bellefair', serif;
+    --heading4: 32px 'Bellefair', serif;
+    --heading5: 28px 'Barlow Condensed', sans-serif;
+    --subHeading1: 28px 'Bellefair', serif;
+    --subHeading2: 14px 'Barlow Condensed', sans-serif;
+    --navText: 16px 'Barlow Condensed', sans-serif;
+    --bodyText: sans-serif
+  }
+
+  h1 {
+    color: var(--darkBlue);
+  }
+`;
