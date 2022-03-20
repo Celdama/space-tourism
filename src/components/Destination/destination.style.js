e23c81a8c4bdc857a9b1ab1dbaae1a2b102060ec
Celdama <celdama.dev@gmail.com>
2022-03-20 07:44:13 +0100

    components: use grid to layout destination content

--- src/components/Destination/destination.style.js
+++ src/components/Destination/destination.style.js
@@ -6,6 +6,101 @@ export const Wrapper = styled.div`
   background-size: cover;
   display: flex;
   align-items: center;
+
+  .grid {
+    width: 1047px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: 1fr 0.5fr repeat(4, 1fr);
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    color: white;
+  }
+
+  .div1 {
+    grid-area: 1 / 1 / 2 / 2;
+    h1 {
+      text-transform: uppercase;
+      font: var(--heading5);
+      letter-spacing: 4.725px;
+    }
+  }
+  .div2 {
+    grid-area: 2 / 2 / 3 / 3;
+    margin-left: 40px;
+    ul {
+      display: flex;
+      list-style: none;
+      padding: 0;
+      gap: 10px;
+      font: var(--navText);
+      letter-spacing: 2.7px;
+      line-height: 19px;
+      color: var(--purple);
+    }
+  }
+  .div3 {
+    grid-area: 2 / 1 / 7 / 2;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    img {
+      width: 445px;
+      height: 445px;
+    }
+  }
+  .div4 {
+    grid-area: 3 / 2 / 7 / 3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 472px;
+    width: 445px;
+    margin-left: 40px;
+
+    h2 {
+      font: var(--heading2);
+      text-transform: uppercase;
+      padding: 0;
+      margin: 0;
+      line-height: 115px;
+    }
+
+    p {
+      font: var(--bodyText);
+      line-height: 32px;
+      color: var(--purple);
+    }
+    .divider {
+      height: 1px;
+      background-color: #fff;
+      opacity: 0.25;
+      z-index: 10;
+      margin-top: 40px;
+    }
+    .info {
+      display: flex;
+
+      .distance,
+      .travel {
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+
+        .title {
+          font: var(--subHeading2);
+          text-transform: uppercase;
+          color: var(--purple);
+        }
+
+        .data {
+          font: var(--subHeading1);
+          text-transform: uppercase;
+        }
+      }
+    }
+  }
 `;
 
 export const Content = styled.div`
--- src/components/Destination/index.js
+++ src/components/Destination/index.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Wrapper, Content } from './destination.style';
 import bg from '../../assets/destination/background-destination-desktop.jpg';
 import data from '../../data/data.json';
-import { Planet } from '../Planet';
-
-console.log(bg);
 
 export const Destination = () => {
   const [destinations, setDestinations] = useState(data.destinations);
@@ -16,10 +13,43 @@ export const Destination = () => {
 
   return (
     <Wrapper bg={bg}>
-      <Content>
+      {/* <Content>
         <h1>01 pick your destination</h1>
         <Planet destination={currentDestination} />
-      </Content>
+      </Content> */}
+      {currentDestination && (
+        <div className='grid'>
+          <div className='div1'>
+            <h1>01 pick your destination</h1>
+          </div>
+          <div className='div2'>
+            <ul>
+              <li>MOON</li>
+              <li>MARS</li>
+              <li>EUROPA</li>
+              <li>TITAN</li>
+            </ul>
+          </div>
+          <div className='div3'>
+            <img src={currentDestination.images?.png} alt='' />
+          </div>
+          <div className='div4'>
+            <h2>{currentDestination.name}</h2>
+            <p>{currentDestination.description}</p>
+            <div className='divider'></div>
+            <div className='info'>
+              <div className='distance'>
+                <span className='title'>avg. distance</span>
+                <span className='data'>{currentDestination.distance}</span>
+              </div>
+              <div className='travel'>
+                <span className='title'>est. travel time</span>
+                <span className='data'>{currentDestination.travel}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </Wrapper>
   );
 };
